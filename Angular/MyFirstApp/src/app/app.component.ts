8cb085e686251ff0b5b52b494da3cce5d96175fe
Rupadevi900 <kandipallyrupa8@gmail.com>
2022-09-21 12:03:16 +0530

    routing

--- Angular/MyFirstApp/src/app/app.component.ts
+++ Angular/MyFirstApp/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'MyFirstApp';
+
+  /* Data binding in angular:-
+  1. one way binding ---> {{ }} (string interpolation)--> model to view
+  2. property binding -->[ ]--> model to view
+  3. event binding -->()-->view to model
+  4. 2 way binding--> [( )]--> model to view & vice-versa
+  5. attribute binding -->attrib.attributeName-->model to view
+
+*/
+firstName = "rupa";
+colSpanValue="2"
+
+lastName: string = "devi";
+
+showMessage(){
+  console.log("this is showMessage");
+}
 }
